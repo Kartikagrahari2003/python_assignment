15e7dba0a80f5b6321e788893b1e7650253a7fac
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-02 21:06:55 +0530

    String swaping

---
+++ 02_string_swaping.py
@@ -0,0 +1,15 @@
+# Q. 2. Switch values of two strings
+char1 = "Hello"
+char2 = "Java"
+
+# Printing data type
+print(type(char1), type(char2))
+
+# ext = external vsriables for store data
+ext = char1
+char1= char2
+char2= ext
+print(f"{char1},{char2}")
+
+
+
