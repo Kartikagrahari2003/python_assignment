d0c92044b45f45b57c3d316e518442b912ce2a0e
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-02 21:29:35 +0530

    Update balance

---
+++ 04_mini_bank.py
@@ -0,0 +1,6 @@
+# Q4. Update balance after deposit
+initial= 10000 
+print(f"Your initial ammont is: {initial} Rs")
+deposit=float(input("Enter your deposit amount:"))
+deposit= deposit+ initial
+print(f"Your remainig ammount::: {deposit} Rs")
