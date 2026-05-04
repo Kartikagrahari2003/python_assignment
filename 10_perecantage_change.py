baa483804b15efd1d5952d26b369807a8f304fed
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-04 14:35:45 +0530

    # Calculate percentage change

---
+++ 10_perecantage_change.py
@@ -0,0 +1,13 @@
+# Calculate percentage  change
+
+x = float(input("Enter first number:::"))
+y = float(input("Enter final number:::"))
+#  Formula of percentage change
+if x <= y:
+    a = (y-x)/x*100
+    print(f"Your percentage profit change is {a}%")
+elif x >= y:
+    a = (y-x)/x *100
+    print(f"Your percentage loss change is {a}%")
+
+
