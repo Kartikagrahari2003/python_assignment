51acb54d0d50375d459027939bc88c1309ea6d94
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-04 11:25:06 +0530

    Simple intrest Calculator

---
+++ 07_simple_intrest.py
@@ -0,0 +1,12 @@
+# Simple intrest Calculatop
+
+print("Simple Intrest Calculater😊")
+
+x = float(input("Principal:::"))
+y = float(input("Rate:::"))
+z= int(input("Enter tenure in years:::"))
+
+a= (x*y*z)/100
+b= a+x
+print(f"total intrest{a}")
+print(f"Total payable amount:::{b}")
