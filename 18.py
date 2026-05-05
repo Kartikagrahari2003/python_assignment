a4d9367619b39eb1e2dc82be66acbc6c424c7cd1
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-05 22:27:20 +0530

    Evaluate: (a + b)(a - b)

---
+++ 18.py
@@ -0,0 +1,8 @@
+# Evaluate: (a + b)(a - b) 
+
+print("If your question in a**2 + b**2 or (a+b)(a-b) form")
+a= float(input("Enter value of a = "))
+b= float(input("Enter value of b = "))
+
+c = (a**2)-(b**2)
+print(f"Answer = ", {c})
