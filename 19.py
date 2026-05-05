961f9a63a9d0c8bc939fdb521c9fcebe1bbe9626
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-05 22:33:22 +0530

    Sum of cubes: a³ + b³

---
+++ 19.py
@@ -0,0 +1,9 @@
+# Sum of cubes: a³ + b³ 
+print("If your question is a**3 + b**3 form...")
+a= float(input("Enter value of a = "))
+b= float(input("Enter value of b = "))
+
+c = (a**3)+(b**3)
+
+print(f"Answer =", {c})
+
