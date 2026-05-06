8d5c16ae37582c492cb1a7a37f93c41bb353ce6a
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-06 11:32:02 +0530

    Ek number even hai ya odd check karo.

---
+++ 20.py
@@ -0,0 +1,1 @@
+# Simplify: (x - y)² 
---
+++ new.py
@@ -0,0 +1,10 @@
+# Ek number even hai ya odd check karo.
+
+print("Even and Odd checker")
+a= int(input("Enter first number:::"))
+
+
+if a % 2==0:
+    print(f"Number is even = {a}")
+else:
+    print(f"Number is odd = {a}")
